043bdd8bfc0ed4370f6410a1dcdffd579b15bcfe
amit1208 <amitrajput.chauhan@gmail.com>
2024-05-16 15:08:19 +0530

    updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,6 @@ const config: Config = {
       }
     },
   },
-  plugins: [addVariablesForColors, addSvgPatterns],
+  plugins: [addVariablesForColors,require("daisyui"), addSvgPatterns],
 };
 export default config;
